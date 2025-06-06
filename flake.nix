4b1f1318eb3747b95a68941eff2bc14e4a4356e4
Parker Jones <parker.jones@rithum.com>
2025-06-06 15:01:45 -0400

    debugging flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
 {
-  description = "crosskey: a global key-overlay for highlighting Vim-style motions";
+  description = "crosskey: a global key‐overlay for highlighting Vim‐style motions";
 
   inputs = {
-    # We pin to a recent nixpkgs so that buildRustPackage still works,
-    # but we want a newer Rust too (for edition2024).
+    # We pin to a recent nixpkgs, but also pull in rust-overlay for a newer Rust toolchain.
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url  = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
@@ -12,65 +11,55 @@
   outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # Import nixpkgs for this system, with rust-overlay overlaid:
+        # 1) Import nixpkgs with the rust-overlay applied
         overlayedPkgs = import nixpkgs {
           inherit system;
           overlays = [ (import rust-overlay { inherit system; }).overlay ];
         };
 
-        # Now the overlayedPkgs has a modern rustPlatform, rustc, cargo, etc.
-        rustPkgs = overlayedPkgs;
+        rustPkgs     = overlayedPkgs;
         rustPlatform = rustPkgs.rustPlatform;
 
-        # Build the crosskey Rust package:
+        # 2) Build the crosskey Rust package using the newer Rust
         crosskeyPackage = rustPlatform.buildRustPackage rec {
-          pname = "crosskey";
-          version = "0.1.0"; # match your Cargo.toml
+          pname    = "crosskey";
+          version  = "0.1.0";  # match your Cargo.toml
 
-          src = ./.;  # this directory (contains Cargo.toml & Cargo.lock)
-
-          # Do not specify cargoLock = ...; newer buildRustPackage auto-detects it.
-          cargoSha256 = "0000000000000000000000000000000000000000000000000000";
+          src         = ./.;   # point at the directory containing Cargo.toml & Cargo.lock
+          cargoSha256 = "0000000000000000000000000000000000000000000000000000";  
+                         # placeholder: replace with the hash from the first `nix build` run
 
           nativeBuildInputs = [ rustPkgs.pkg-config ];
           buildInputs       = [ ];
-
-          # If you need X11 at runtime (Linux), you could add:
-          # buildInputs = [ rustPkgs.libX11 rustPkgs.libxcb ];
         };
       in
       {
-        # Expose the package so that people can do:
-        #    nix profile install .#crosskey
-        packages = {
-          crosskey = crosskeyPackage;
+        ### 3) Expose the package so people can do:
+        ###     nix profile install .#crosskey
+        packages.crosskey = crosskeyPackage;
+
+        ### 4) Expose it as an “app” so people can run:
+        ###     nix run .#crosskey
+        apps.default = {
+          type    = "app";
+          program = "${crosskeyPackage}/bin/crosskey";
         };
 
-        # Expose it as an “app” so people can run:
-        #    nix run .#crosskey
-        apps = {
-          default = {
-            type = "app";
-            program = "${crosskeyPackage}/bin/crosskey";
-          };
-        };
+        ### 5) Export a development shell under `devShells.<system>.default`
+        ###    instead of a top‐level `devShell = …`; this fixes the “expected a set but found a function” error.
+        devShells.default = rustPkgs.mkShell {
+          buildInputs = [
+            rustPkgs.rustc
+            rustPkgs.cargo
+            rustPkgs.rustfmt
+            rustPkgs.rust-analyzer
+            rustPkgs.pkg-config
+          ];
 
-        # Expose a development shell under `devShells.${system}.default`
-        devShells = {
-          default = rustPkgs.mkShell {
-            buildInputs = [
-              rustPkgs.rustc
-              rustPkgs.cargo
-              rustPkgs.rustfmt
-              rustPkgs.rust-analyzer
-              rustPkgs.pkg-config
-            ];
-
-            shellHook = ''
-              export RUST_SRC_PATH="${rustPkgs.rustc}/lib/rustlib/src/rust/library"
-              echo "Entering crosskey devShell (rustc: $(rustc --version))"
-            '';
-          };
+          shellHook = ''
+            export RUST_SRC_PATH="${rustPkgs.rustc}/lib/rustlib/src/rust/library"
+            echo "Entering crosskey devShell (rustc: $(rustc --version))"
+          '';
         };
       }
     );
